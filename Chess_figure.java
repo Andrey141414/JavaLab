07a5597a86f1bea5fda183b4d97a1b5ecb157b3c
Andrey141414 <andrusha.vinokurov@gmail.com>
2021-01-28 02:48:14 +0700

    LAB #9

--- Chess_figure.java
+++ Chess_figure.java
@@ -1,59 +1,38 @@
 import java.util.Scanner;
 public class Chess_figure{
-private String[] NAMES = {"Пешка","Конь", "Слон", "Ладья", "Ферзь"};
+
 private String name;
 
 public int value;
 public boolean pawn;
 
 
-public void Read(){
-
-	int vibor;
-	System.out.println("Название фигуры");
-	for (int i = 0; i < 5; i++)
-	{
-		System.out.printf("%d ",i+1);
-		System.out.println(NAMES[i]);
-
-	}
-
-	Scanner scan = new Scanner(System.in);
-	vibor = scan.nextInt();
-
-	name = NAMES[vibor - 1];
-	switch (vibor)
-	{
-	case 1: {value = 1; pawn = true; }break;
-	case 2: {value = 3; }break;
-	case 3: {value = 3; }break;
-	case 4: {value = 5; }break;
-	case 5: {value = 9; }break;
-	}
-	
-
+public Chess_figure(String name,int value)
+{
+this.name = name;
+this.value = value;
 }
 
-public void Init(int val,String nam){
-value = val;
-	name = nam;
-	if (name == NAMES[0])
-		pawn = true;
-	else
-		pawn = false;
+public Chess_figure(String name)
+{
+this.name = name;
+value = 3;
+}
+
+public Chess_figure()
+{
+this.name = "King";
+this.value = 10;
 }
 
 public void Display(){
 
-System.out.printf(" %s  Ценность - ",name);
+System.out.printf(" %s  Value - ",name);
 System.out.println(value);
 }
 
 
-public void Compare(Help a)
-{
-a.Help_name = name;
-}
+
 
 
 }
--- Program.java
+++ Program.java
@@ -1,76 +1,39 @@
+import java.util.Scanner;
 public class Program {
-public void clear()
-{
-	for(int i=0;i<50;i++)
-	{
-	System.out.println();
-	}
-}
-
-
 
 	public static void main(String[] args) {
-	Program screen = new Program();
-	String[] NAMES = {"Pawn","Knight", "Bishop", "Rook", "Queen"};
-	int N = 10;
-		Chess_figure[] MAS = new Chess_figure[N];
-
-	for(int i=0;i<N;i++)
-	{
-		MAS[i] = new Chess_figure();
-		switch (i%5)
-		{
-		case 0: {MAS[i].Init(1, NAMES[i % 5]); }; break;
-		case 1: {MAS[i].Init(3, NAMES[i % 5]); }; break;
-		case 2: {MAS[i].Init(3, NAMES[i % 5]); }; break;
-		case 3: {MAS[i].Init(5, NAMES[i % 5]); }; break;
-		case 4: {MAS[i].Init(9, NAMES[i % 5]); }; break;
-		}
 	
-	}
+	Chess_figure a = new Chess_figure("Ladya",5);
+	Chess_figure b = new Chess_figure("Slon");
+	Chess_figure c = new Chess_figure();
 	
-	for(int i= 0;i<N;i++)
-	{
-	MAS[i].Display();
-	}
 
 
+	a.Display();
+	b.Display();
+	c.Display();
+	
+	int N = 5;
+	Chess_figure[] MAS = new Chess_figure[N];
+	
+for (int i=0;i<N;i++)
+{
+Chess_figure buff = new Chess_figure("Ladya");
+MAS[i] = buff;
+}
 
-	for(int i=0;i < N;i++)
-	{
-	Help a = new Help();
-	MAS[i].Compare(a);
+MAS[1].value = 4;
 
-	for(int j = i+1;j < N+1;j++)
-	{	
-	Help b = new Help();
-	MAS[j].Compare(b);
-	if(a.Help_name.equalsIgnoreCase(b.Help_name))
-		{
-		for(int z = j;z < N-1;z++)
-			{
-			MAS[z]=MAS[z+1];
-			}
-		N-=1;
-		}
-	}
-	} 
+System.out.printf("\n\nMassive\n\n");
 
 
-
-	screen.clear();
-	for(int i= 0;i<N;i++)
-	{
-	MAS[i].Display();
-	}
-
-
-
-screen.clear();
-Chess_Player Magnus = new Chess_Player();
-	Magnus.playerInit("Magnus",N, MAS, 1);
-
-Magnus.playerDisplay();
-
+for(int i=0;i<N;i++)
+{
+MAS[i].Display();
+}
+	
 }
 }
+
+
+
