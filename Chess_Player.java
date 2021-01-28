a7f50a2705f345ae9b36a22f4e473ea1a9164af7
Andrey141414 <andrusha.vinokurov@gmail.com>
2021-01-28 19:07:58 +0700

    Lab #10

--- Chess_Player.java
+++ Chess_Player.java
@@ -1,51 +1,34 @@
 import java.util.Scanner;
 public class Chess_Player{
 
-//колличество фигур
+
 private int numbers;
 private String PlayerName;
-//РАзряд шахматиста
 private int rank;
 
 
 
-	//сами фигуры
 public Chess_figure[] figures = new Chess_figure[16];
 public boolean color =true;
 
 
-	//функция установки значений
-public void playerRead(){
-	System.out.println("Имя игрока");
-	Scanner scan1 = new Scanner(System.in);
-	PlayerName = scan1.nextLine();
 	
-	System.out.println("Укажите разряд игрока");
-	Scanner scan2 = new Scanner(System.in);
-	rank = scan2.nextInt();
-	
-	System.out.println("Укажите колличество фигур");
-	Scanner scan3 = new Scanner(System.in);
-	numbers = scan3.nextInt();
 
-	for (int i = 0; i < numbers; i++)
-	{
-		System.out.printf("\tВвод фигур\nФигур осталось %d\n",numbers-i);
-		figures[i].Read();
-		
-	}
-}
 	//функция получения значений
-public void playerInit(String PlayerName, int numbers, Chess_figure[] cf, int rank){
-	this.PlayerName = PlayerName;
-	this.numbers = numbers;
+public Chess_Player(String Pn, int numb, Chess_figure[] cf, int razryad){
+
+	rank = razryad;
+	PlayerName = Pn;
+	numbers = numb;
 	for (int i = 0; i < numbers; i++)
 	{
 		figures[i] = cf[i];
 	}
-	this.rank = rank;
 	color = false;
 }
+
+
+
 	//функция вывода
 public void playerDisplay(){
 System.out.printf("Name -  %s \n",PlayerName);
@@ -63,13 +46,10 @@ System.out.printf("Name -  %s \n",PlayerName);
 		System.out.printf("%d ",i + 1);
 		figures[i].Display();
 	}
-}
-
-public void playerCompare(Chess_Player player){
 
 }
+
 	
-
 
 
 
--- Program.java
+++ Program.java
@@ -1,39 +1,23 @@
-import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-	
-	Chess_figure a = new Chess_figure("Ladya",5);
-	Chess_figure b = new Chess_figure("Slon");
-	Chess_figure c = new Chess_figure();
-	
-
-
-	a.Display();
-	b.Display();
-	c.Display();
-	
-	int N = 5;
+	int N = 10;
 	Chess_figure[] MAS = new Chess_figure[N];
 	
-for (int i=0;i<N;i++)
-{
-Chess_figure buff = new Chess_figure("Ladya");
-MAS[i] = buff;
-}
-
-MAS[1].value = 4;
-
-System.out.printf("\n\nMassive\n\n");
-
-
-for(int i=0;i<N;i++)
-{
-MAS[i].Display();
-}
 	
+//exception(too big size of massive)
+	try
+	{	
+		for (int i=0;i<N+1;i++)
+		{
+		Chess_figure buff = new Chess_figure("Ladya");
+		MAS[i] = buff;
+		}
+	}
+	  catch(ArrayIndexOutOfBoundsException e)  // исключение – выход за границу массива
+  	{
+  	 System.out.printf("неправильный индекс массива: " + e); // e - сообщение класса exception
+  	}
+
 }
 }
-
-
-
